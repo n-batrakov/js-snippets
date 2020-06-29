4e530a0381ffe9944b0dd18a1e85f9646ad0fa50
n-batrakov <n.m.batrakov@gmail.com>
2020-06-29 13:41:43 +0300

    Add fold

--- src/fns/unfold.spec.ts
+++ src/fns/unfold.spec.ts
@@ -1,3 +1,19 @@
+const fold = (nested: any): any => Object.entries(nested).reduce(
+    (acc, [k, v]) => {
+      if (v instanceof Date || typeof v !== 'object' || Array.isArray(v)) {
+        acc[k] = v
+        return acc
+      }
+
+      Object.entries(fold(v)).forEach(([k1, v1]) => {
+          acc[`${k}.${k1}`] = v1
+      })
+
+      return acc
+    },
+    {} as any,
+)
+
 const unfold = (plain: any) => {
     let nested: any = {}
 
@@ -14,6 +30,29 @@ const unfold = (plain: any) => {
 
     return nested
 }
+
+
+describe('fold', () => {
+    it('fold nested', () => {
+        let init = {
+            a: 1,
+            b: { a: 1, b: 2 },
+            c: { a: { a: 1, b: 2 } },
+        }
+
+        let expected = {
+            a: 1,
+            'b.a': 1,
+            'b.b': 2,
+            'c.a.a': 1,
+            'c.a.b': 2,
+        }
+
+        let actual = fold(init)
+
+        expect(actual).toEqual(expected)
+    })
+})
 
 describe('unfold', () => {
     it('unfold nested', () => {
